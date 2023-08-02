131c63118d2d576ec5ab3a9474d0b30b7a757531
savperera369 <90350791+savperera369@users.noreply.github.com>
2023-08-02 03:39:12 +0000

    first commit

---
+++ front-end/src/pages/ProductsPage.vue
@@ -0,0 +1,20 @@
+<template>
+  <h1>Products</h1>
+  <ProductsList :products="products" />
+</template>
+
+<script>
+import {products} from '../temp-data';
+import ProductsList from '../components/ProductsList.vue'
+export default {
+  name: "ProductsPage",
+  components: {
+    ProductsList,
+  },
+  data() {
+    return {
+      products, 
+    }
+  }
+};
+</script>
---
+++ front-end/src/pages/ShoppingCartPage.vue
@@ -0,0 +1,27 @@
+<template>
+  <h1>Shopping Cart</h1>
+  <div v-if="cartItems.length>0">
+      <ShoppingCartList :products="cartItems" />
+  </div>
+  <div v-if="cartItems.length===0">
+    <p>You have no items in your cart!</p>
+  </div>
+  <button class="checkout-button">Proceed to Checkout</button>
+</template>
+
+<script>
+import ShoppingCartList from '@/components/ShoppingCartList.vue';
+import { cartItems } from '../temp-data';
+// v-for repeats the div for each product in the cardItem array
+export default {
+  name: "ShoppingCartPage",
+  components: {
+    ShoppingCartList
+  },
+  data () {
+    return {
+      cartItems,
+    }
+  }
+};
+</script>
