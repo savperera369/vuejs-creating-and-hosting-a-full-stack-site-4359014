437ab8bb5af44d22e3b1464f8979cf670bcb39c0
savperera369 <saviru2002@outlook.com>
2023-08-04 00:43:43 -0400

    firebase sign in plus login

--- front-end/src/main.js
+++ front-end/src/main.js
@@ -7,6 +7,24 @@ import ProductsPage from './pages/ProductsPage.vue'
 import ProductDetailPage from './pages/ProductDetailPage.vue'
 import NotFoundPage from './pages/NotFoundPage.vue'
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDaAEFeUthLjylBlxUxm2ovK8vQdhKQwc0",
+  authDomain: "vue-site-b0d04.firebaseapp.com",
+  projectId: "vue-site-b0d04",
+  storageBucket: "vue-site-b0d04.appspot.com",
+  messagingSenderId: "356046739184",
+  appId: "1:356046739184:web:164aa9b2c0483c5fea5435"
+};
+
+// Initialize Firebase
+initializeApp(firebaseConfig);
+
 createApp(App)
 .use(VueRouter.createRouter({
   history: VueRouter.createWebHistory(process.env.BASE_URL),
--- front-end/src/pages/ProductDetailPage.vue
+++ front-end/src/pages/ProductDetailPage.vue
@@ -8,6 +8,7 @@
       <h3 class="price">{{ product.price }}</h3>
       <button @click="addToCart" class="add-to-cart" v-if="!itemIsInCart">Add to Cart</button>
       <button class="grey-button" v-if="itemIsInCart">Item is already in cart</button>
+      <button class="sign-in" @click="signIn">Sign in to add to cart</button>
     </div>
   </div>
   <div v-if="!product">
@@ -18,6 +19,7 @@
 
 <script>
 import axios from 'axios';
+import { getAuth, sendSignInLinkToEmail, isSignInWithEmailLink, signInWithEmailLink} from 'firebase/auth';
 import NotFoundPage from '@/pages/NotFoundPage.vue';
 export default {
   name: "ProductDetailPage",
@@ -33,6 +35,21 @@ export default {
       // request body is given in form of a JS object
       await axios.post('/api/users/12345/cart', {id: this.$route.params.productId});
       alert('Successfully added item to cart');
+    }, 
+    async signIn(){
+      const email = prompt("Please enter your email to sign in: ");
+      // reference to firebase auth object
+      const auth = getAuth();
+      // settings object that tells firebase auth how to redirect the user
+      const actionCodeSettings = {
+        // this is the url we want to redirect the user back to when they log in
+        url: `http://localhost:8080/products/${this.$route.params.productId}`,
+        handleCodeInApp: true,
+      }
+      await sendSignInLinkToEmail(auth, email, actionCodeSettings);
+      alert("Check your email!");
+      // store the users email that they gave us for later so that we can use it to sign in when the user clicks the link
+      window.localStorage.setItem('emailForSignIn', email);
     }
   },
   components: {
@@ -44,6 +61,14 @@ export default {
     }
   },
   async created(){
+    // add logic to check to see if the url indicates that the user got to this page using an email link
+    const auth = getAuth();
+    if (isSignInWithEmailLink(auth, window.location.href)){
+      const email = window.localStorage.getItem('emailForSignIn');
+      await signInWithEmailLink(auth, email, window.location.href);
+      alert('Successfully signed in!');
+      window.localStorage.removeItem('emailForSignIn');
+    }
     const response = await axios.get(`/api/products/${this.$route.params.productId}`);
     const product = response.data;
     this.product = product;
